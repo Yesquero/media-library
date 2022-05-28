65c190ae3f5cb7f6f4621d828001464707e61064
Yesquero <yesquero1986@gmail.com>
2022-05-28 16:11:46 +0300

    Domain setup

--- app/src/main/java/ru/mirea/medlib/database/MedLibDatabase.kt
+++ app/src/main/java/ru/mirea/medlib/database/MedLibDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [MediaEntity::class, Episode::class], version = 1)
-@TypeConverters(CountryConverter::class, GenreConverter::class, )
+@TypeConverters(CountryConverter::class, GenreConverter::class)
 abstract class MedLibDatabase : RoomDatabase() {
     abstract val mediaEntityDao: MediaEntityDao
 }
--- app/src/main/java/ru/mirea/medlib/repository/MediaLibraryRepository.kt
+++ app/src/main/java/ru/mirea/medlib/repository/MediaLibraryRepository.kt
@@ -2,9 +2,14 @@ package ru.mirea.medlib.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import ru.mirea.medlib.database.MedLibDatabase
+import ru.mirea.medlib.database.asDomainModel
 import ru.mirea.medlib.domain.MediaDetails
 import ru.mirea.medlib.network.KinopoiskService
+import ru.mirea.medlib.network.ResultWrapper
+import ru.mirea.medlib.network.dto.FilmSearchDto
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,7 +20,16 @@ class MediaLibraryRepository @Inject constructor(
 ) : BaseRepository("MedialLibraryRepository") {
 
     val library: LiveData<List<MediaDetails>> =
-        Transformations.map(database.mediaEntityDao.getAllWithEpisode()) { it ->
-            it.asDomainModel()
+        Transformations.map(database.mediaEntityDao.getAllWithEpisode()) { list ->
+            list.map { it ->
+                it.asDomainModel()
+            }
         }
+
+    suspend fun keywordSearch(keyword: String): Flow<ResultWrapper<List<FilmSearchDto>>> {
+        return flow {
+            emit(ResultWrapper.Loading())
+            emit(safeApiCall { kinopoiskService.searchByKeyword(keyword, 1) })
+        }
+    }
 }
